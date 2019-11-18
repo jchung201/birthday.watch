06620342dcf8dc49c25c9aa613f71f0515e497c8
jchung201 <jchung201@gmail.com>
2019-11-18 14:07:01 -0500

    (Auth) Received auth url.

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
 import asyncHandler from "express-async-handler";
-const { authorize } = require("../services/authorize");
+const { getAuthUrl } = require("../services/auth");
+
 const fs = require("fs");
 const { calendarCheck } = require("../services/calendarCheck");
 import { credentials } from "../lib/credentials.js";
 
-router.post(
+router.get(
   "/",
   asyncHandler(async (req, res, next) => {
-    console.log(credentials);
-    authorize(credentials, calendarCheck, data => {});
+    res.send(await getAuthUrl(credentials));
   })
 );
 
---
+++ src/services/auth.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const readline = require("readline");
+const { google } = require("googleapis");
+// If modifying these scopes, delete token.json.
+const SCOPES = [
+  "https://www.googleapis.com/auth/calendar.events",
+  "https://www.googleapis.com/auth/calendar"
+];
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+async function getAuthUrl(credentials) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+  const authUrl = await oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES
+  });
+  return authUrl;
+}
+
+module.exports = {
+  getAuthUrl
+};
