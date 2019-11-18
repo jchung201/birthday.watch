e53f91990cfb0555b76d5b2d146f3858f88c7e38
jchung201 <jchung201@gmail.com>
2019-11-18 17:01:59 -0500

    (Create Event) Set up and ready.

--- src/controllers/birthdays.js
+++ src/controllers/birthdays.js
@@ -1,15 +1,17 @@
 const router = require("express").Router();
 import asyncHandler from "express-async-handler";
 const { listEvents } = require("../services/listEvents");
+const { createEvent } = require("../services/createEvent");
 const { calendarCheck } = require("../services/calendarCheck");
 
 router
   .get(
     "/",
     asyncHandler(async (req, res, next) => {
-      await calendarCheck(req.body.token, (err, calendar) => {
+      const { token } = req.body;
+      await calendarCheck(token, (err, calendar) => {
         if (err) return next(err);
-        listEvents(req.body.token, calendar.id, (err, events) => {
+        listEvents(token, calendar.id, (err, events) => {
           if (err) return next(err);
           res.send(events);
         });
@@ -19,6 +21,14 @@ router
   .post(
     "/",
     asyncHandler(async (req, res, next) => {
+      await calendarCheck(req.body.token, (err, calendar) => {
+        const { token, date } = req.body;
+        if (err) return next(err);
+        createEvent(token, calendar.id, date, (err, events) => {
+          if (err) return next(err);
+          res.send(events);
+        });
+      });
       console.log("hi");
     })
   );
--- src/services/createEvent.js
+++ src/services/createEvent.js
@@ -1,7 +1,7 @@
 const { google } = require("googleapis");
 import { credentials } from "../lib/credentials.js";
 
-const createEvent = async (auth, calendarId, dates) => {
+const createEvent = async (token, calendarId, date, cb) => {
   const { client_secret, client_id, redirect_uris } = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
     client_id,
@@ -10,37 +10,39 @@ const createEvent = async (auth, calendarId, dates) => {
   );
   oAuth2Client.setCredentials(JSON.parse(JSON.stringify(token)));
   const calendar = google.calendar({ version: "v3", auth: oAuth2Client });
-  for (let i = 0; i < dates.length; i++) {
-    const { date, name, description } = dates[i];
-    const first = new Date(date);
-    const second = new Date(first);
-    second.setDate(first.getDate() + 1);
-    const summary = `${name}'s Birthday!`;
-    var event = {
-      summary: summary,
-      location: "Birthday",
-      description: description,
-      start: {
-        dateTime: first,
-        timeZone: "America/New_York"
-      },
-      end: {
-        dateTime: second,
-        timeZone: "America/New_York"
-      },
-      recurrence: ["RRULE:FREQ=YEARLY"],
-      reminders: {
-        useDefault: false,
-        overrides: [{ method: "email", minutes: 168 * 60 }]
-      }
-    };
-    await calendar.events.insert({
-      auth: auth,
-      calendarId: calendarId,
-      resource: event
-    });
-    console.log(name);
-  }
+
+  console.log(date, calendarId);
+  // for (let i = 0; i < dates.length; i++) {
+  //   const { date, name, description } = dates[i];
+  //   const first = new Date(date);
+  //   const second = new Date(first);
+  //   second.setDate(first.getDate() + 1);
+  //   const summary = `${name}'s Birthday!`;
+  //   var event = {
+  //     summary: summary,
+  //     location: "Birthday",
+  //     description: description,
+  //     start: {
+  //       dateTime: first,
+  //       timeZone: "America/New_York"
+  //     },
+  //     end: {
+  //       dateTime: second,
+  //       timeZone: "America/New_York"
+  //     },
+  //     recurrence: ["RRULE:FREQ=YEARLY"],
+  //     reminders: {
+  //       useDefault: false,
+  //       overrides: [{ method: "email", minutes: 168 * 60 }]
+  //     }
+  //   };
+  //   await calendar.events.insert({
+  //     auth: auth,
+  //     calendarId: calendarId,
+  //     resource: event
+  //   });
+  //   console.log(name);
+  // }
 };
 module.exports = {
   createEvent
