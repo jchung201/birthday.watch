74e53f498ebbc87688daa6fc51475c44933ba707
jchung201 <jchung201@gmail.com>
2019-11-19 15:24:15 -0500

    (ENV) Included a process env port.

--- src/server.js
+++ src/server.js
@@ -12,6 +12,6 @@ app.use((req, res, next) => {
 import errorHandler from './lib/errorHandler';
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listening on port: ' + 3000);
+app.listen(process.env.PORT, () => {
+  console.log('Listening on port: ' + process.env.PORT);
 });
