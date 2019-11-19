f0668dbff1a2572a21e982c233d36bf273bbdf2d
jchung201 <jchung201@gmail.com>
2019-11-19 15:19:26 -0500

    (404/dist) Added 404 catchall and fixed dist issue.

--- src/lib/errorHandler.js
+++ src/lib/errorHandler.js
@@ -1,4 +1,7 @@
 export default (error, req, res, next) => {
   console.log(error);
+  if (error.status) {
+    return res.status(error.status).json(error);
+  }
   res.status(500).json({ status: 500, message: 'I apologize for this error!' });
 };
--- src/server.js
+++ src/server.js
@@ -4,6 +4,11 @@ import app from './lib/app';
 const router = require('./routes/index');
 app.use('/rest', router);
 
+// Catch all if not found
+app.use((req, res, next) => {
+  next({ status: 404, message: '404 Not Found' });
+});
+
 import errorHandler from './lib/errorHandler';
 app.use(errorHandler);
 
