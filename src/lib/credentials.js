1f36f152efc3d760bf8f8d47c6595faa436759e1
jchung201 <jchung201@gmail.com>
2020-01-15 23:14:57 -0500

    (ENV) Added dynamic env values.

--- src/lib/credentials.js
+++ src/lib/credentials.js
@@ -2,15 +2,14 @@ require('dotenv').config();
 const credentials = {
   installed: {
     client_id: process.env.CLIENT_ID,
-    project_id: 'quickstart-1573584819309',
+    project_id: process.env.project_id,
     auth_uri: 'https://accounts.google.com/o/oauth2/auth',
     token_uri: 'https://oauth2.googleapis.com/token',
     auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
     client_secret: process.env.CLIENT_SECRET,
-    redirect_uris: ['urn:ietf:wg:oauth:2.0:oob', 'http://localhost']
+    redirect_uris: [`${process.env.WEB_URL}/token`]
   }
 };
-
 module.exports = {
   credentials
 };
