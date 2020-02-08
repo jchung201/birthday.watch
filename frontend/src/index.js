06677dedc4ee48ffdbc9f1fb31a1a20bca132535
jchung201 <jchung201@gmail.com>
2020-02-08 14:37:21 -0500

    (ENV) URL configured to match frontend.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { API_URL } from "./utilities/API";
+import { API_URL } from "./utilities/URL";
+import axios from "axios";
 
 import HomeContainer from "./components/container/HomeContainer.jsx";
 import CalendarContainer from "./components/container/CalendarContainer.jsx";
@@ -8,7 +9,15 @@ import CalendarContainer from "./components/container/CalendarContainer.jsx";
 class App extends Component {
   state = { loggedIn: true };
   logIn = () => {
-    this.setState({ loggedIn: true });
+    const url = axios
+      .get(`${API_URL}/rest/auth/url`)
+      .then(({ data }) => {
+        console.log(data);
+        this.setState({ loggedIn: true });
+      })
+      .catch(error => {
+        console.error(error);
+      });
   };
   logOut = () => {
     this.setState({ loggedIn: false });
