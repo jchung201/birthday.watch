65e197bc9138bedc0037f16730119c528039e549
jchung201 <jchung201@gmail.com>
2020-02-10 22:46:08 -0500

    (Login) Basic check if loggedin logic written.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,8 +8,45 @@ import CalendarContainer from "./components/container/CalendarContainer.jsx";
 
 class App extends Component {
   state = { loggedIn: true };
+  componentDidMount() {
+    let access_token = localStorage.getItem("access_token");
+    let refresh_token = localStorage.getItem("refresh_token");
+    let scope = localStorage.getItem("scope");
+    let token_type = localStorage.getItem("token_type");
+    let expiry_date = localStorage.getItem("expiry_date");
+    if (access_token && refresh_token && scope && token_type && expiry_date) {
+      this.setState({ loggedIn: true });
+    } else {
+      let search = window.location.search;
+      let params = new URLSearchParams(search);
+      let code = params.get("code");
+      axios
+        .post(`${API_URL}/rest/auth/code`, { code })
+        .then(
+          ({
+            data: {
+              access_token,
+              refresh_token,
+              scope,
+              token_type,
+              expiry_date
+            }
+          }) => {
+            localStorage.setItem("access_token", access_token);
+            localStorage.setItem("refresh_token", refresh_token);
+            localStorage.setItem("scope", scope);
+            localStorage.setItem("token_type", token_type);
+            localStorage.setItem("expiry_date", expiry_date);
+            this.setState({ loggedIn: true });
+          }
+        )
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
   logIn = () => {
-    const url = axios
+    axios
       .get(`${API_URL}/rest/auth/url`)
       .then(({ data }) => {
         localStorage.setItem("credentials", "credentials");
