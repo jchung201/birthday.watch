567d6c172749242ebb2825788a4c8b2ce55bdb2f
jchung201 <jchung201@gmail.com>
2020-02-10 22:33:23 -0500

    (Auth) Fixed google security validation problem.

--- backend/src/utilities/URL.js
+++ backend/src/utilities/URL.js
@@ -1,14 +1,14 @@
-export let API_URL = "";
-export let WEB_URL = "";
+export let API_URL = '';
+export let WEB_URL = '';
 
 switch (process.env.NODE_ENV) {
-  case "production":
-    WEB_URL = "https://birthday.watch";
-    API_URL = "https://api.birthday.watch";
+  case 'production':
+    WEB_URL = 'https://birthday.watch';
+    API_URL = 'https://api.birthday.watch';
     break;
   default:
-    WEB_URL = "http://localhost:8080";
-    API_URL = "http://localhost:3000";
+    WEB_URL = 'http://localhost:8080';
+    API_URL = 'http://localhost:3000';
 }
 
 export default { API_URL, WEB_URL };
