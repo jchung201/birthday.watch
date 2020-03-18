2e264ff184c9d7920c97f02bc15056f6be13b14d
jchung201 <jchung201@gmail.com>
2020-03-17 22:50:01 -0400

    (Store) Fixed.

--- frontend/src/components/container/HomeContainer.jsx
+++ frontend/src/components/container/HomeContainer.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
 import Home from "../presentational/Home.jsx";
-class HomeContainer extends Component {
-  render() {
-    const { logIn } = this.props;
-    return <Home logIn={logIn} />;
-  }
-}
-export default HomeContainer;
+import { observer, inject } from "mobx-react";
+
+// class HomeContainer extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   componentDidMount() {
+//     console.log(this.props);
+//   }
+//   render() {
+//     const { logIn } = this.props;
+//     return <Home logIn={logIn} />;
+//   }
+// }
+export default inject("store")(
+  observer(({ store }) => {
+    console.log(store);
+
+    // store.logIn();
+    console.log(store);
+    return <div>fuck</div>;
+  })
+);
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,23 +1,24 @@
 import { types } from "mobx-state-tree";
 
-const Auth = types.model({
-  loggedIn: types.optional(types.boolean, false),
-  access_token: types.optional(types.string, ""),
-  refresh_token: types.optional(types.string, ""),
-  scope: types.optional(types.string, ""),
-  token_type: types.optional(types.string, ""),
-  expiry_date: types.optional(types.string, "")
-});
-const Calendar = types.model({
-  // birthdays: types.array()
+const Auth = types
+  .model({
+    loggedIn: types.optional(types.boolean, false),
+    access_token: types.optional(types.string, ""),
+    refresh_token: types.optional(types.string, ""),
+    scope: types.optional(types.string, ""),
+    token_type: types.optional(types.string, ""),
+    expiry_date: types.optional(types.string, "")
+  })
+  .actions(self => ({
+    logIn() {
+      self.loggedIn = true;
+    }
+  }));
+
+const Root = types.model({
+  auth: types.optional(Auth, {})
 });
 
-const RootStore = types.model({
-  auth: types.map(Auth),
-  calendar: types.map(Calendar)
-});
-
-export const store = RootStore.create({
-  auth: {},
-  loggedIn: false
+export const store = Root.create({
+  auth: { loggedIn: false }
 });
