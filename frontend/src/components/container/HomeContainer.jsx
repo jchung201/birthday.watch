04addb8d5913ebed4011bd09978d9f371e99920e
jchung201 <jchung201@gmail.com>
2020-03-21 14:45:03 -0400

    (Cleanup) Unused logs/methods.

--- frontend/src/components/container/HomeContainer.jsx
+++ frontend/src/components/container/HomeContainer.jsx
@@ -2,14 +2,5 @@ import React, { Component } from "react";
 import Home from "../presentational/Home.jsx";
 import { observer, inject } from "mobx-react";
 
-class HomeContainer extends Component {
-  componentDidMount() {
-    console.log(this.props);
-  }
-  render() {
-    const { logIn } = this.props;
-
-    return <Home logIn={logIn} />;
-  }
-}
+const HomeContainer = ({ logIn }) => <Home logIn={logIn} />;
 export default inject("store")(observer(HomeContainer));
--- frontend/src/store/Calendar.js
+++ frontend/src/store/Calendar.js
@@ -33,9 +33,7 @@ export const Calendar = types
             }
           })
           .then(({ data }) => {
-            console.log(data);
             self.setBirthdays(data);
-            // this.setState({ dates: data, loading: false });
           })
           .catch(error => {
             console.error(error);
