cd6890346ec3fa6e71998597a7245ff209054cde
jchung201 <jchung201@gmail.com>
2020-03-21 15:30:17 -0400

    (Cleanup) Moved a lot of functionality to store.

--- frontend/src/components/container/CalendarContainer.jsx
+++ frontend/src/components/container/CalendarContainer.jsx
@@ -1,75 +1,26 @@
 import React, { Component } from "react";
 import Calendar from "../presentational/Calendar.jsx";
-import axios from "axios";
-import { API_URL } from "../../utilities/URL";
 import { observer, inject } from "mobx-react";
 
 class CalendarContainer extends Component {
-  state = {
-    dates: [],
-    loading: true,
-    creating: false,
-    name: "",
-    birthDate: "",
-    days: 0,
-    time: "",
-    note: ""
-  };
+  state = { creating: false };
   componentDidMount() {
     this.props.store.calendar.fetchBirthdays();
-    this.fetchDates();
   }
-  fetchDates = () => {
-    const access_token = localStorage.getItem("access_token");
-    const refresh_token = localStorage.getItem("refresh_token");
-    const scope = localStorage.getItem("scope");
-    const token_type = localStorage.getItem("token_type");
-    const expiry_date = localStorage.getItem("expiry_date");
-
-    if (access_token && refresh_token && scope && token_type && expiry_date) {
-      axios
-        .get(`${API_URL}/rest/birthdays/`, {
-          headers: {
-            Authorization: JSON.stringify({
-              access_token,
-              refresh_token,
-              scope,
-              token_type,
-              expiry_date
-            })
-          }
-        })
-        .then(({ data }) => {
-          this.setState({ dates: data, loading: false });
-        })
-        .catch(error => {
-          console.error(error);
-          this.props.logOut();
-        });
-    } else {
-      this.props.logOut();
-    }
-  };
   startCreating = () => {
-    this.setState({ creating: true });
-  };
-  finishCreating = () => {
-    this.setState({ creating: false });
+    console.log("ok");
   };
 
   render() {
     const { logOut } = this.props;
-    const { creating, loading } = this.props;
-    if (loading) return <div>Loading...</div>;
-    console.log(this.props.store.calendar.birthdays[0]);
+    const { birthdays } = this.props.store.calendar;
+    if (birthdays.length < 1) return <div>Loading...</div>;
     return (
       <Calendar
         logOut={logOut}
-        dates={this.state.dates}
-        fetchDates={this.fetchDates}
-        creating={creating}
+        birthdays={birthdays}
+        creating={this.state.creating}
         startCreating={this.startCreating}
-        finishCreating={this.finishCreating}
       />
     );
   }
