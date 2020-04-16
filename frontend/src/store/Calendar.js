c616399f068ddc3e91fa7870a52dc9b65f588bb0
jchung201 <jchung201@gmail.com>
2020-04-16 14:38:35 -0400

    (Production) Logs.

--- frontend/src/store/Calendar.js
+++ frontend/src/store/Calendar.js
@@ -6,41 +6,42 @@ import { checkAuthorization } from "../utilities/auth";
 
 export const Override = types.model({
   method: types.optional(types.string, ""),
-  minutes: types.optional(types.number, 0)
+  minutes: types.optional(types.number, 0),
 });
 export const Reminders = types.model({
   useDefault: types.optional(types.boolean, false),
-  overrides: types.array(Override)
+  overrides: types.array(Override),
 });
 export const Start = types.model({
   dateTime: types.optional(types.string, ""),
-  timeZone: types.optional(types.string, "")
+  timeZone: types.optional(types.string, ""),
 });
 export const Birthday = types.model({
   id: types.string,
   summary: types.optional(types.string, ""),
   start: types.optional(Start, {}),
   reminders: types.optional(Reminders, {}),
-  location: types.optional(types.string, "")
+  location: types.optional(types.string, ""),
 });
 export const Calendar = types
   .model({
     birthdays: types.array(Birthday),
     auth: types.optional(Auth, {}),
-    creating: types.optional(types.boolean, false)
+    creating: types.optional(types.boolean, false),
   })
-  .actions(self => ({
+  .actions((self) => ({
     async fetchBirthdays() {
       const authorization = checkAuthorization(self.auth.logOut);
       try {
         const data = await axios.get(`${API_URL}/rest/birthdays/`, {
           headers: {
-            Authorization: JSON.stringify(authorization)
-          }
+            Authorization: JSON.stringify(authorization),
+          },
         });
+        console.log("fetchData", data);
         self.setBirthdays(data.data);
       } catch (error) {
-        console.error(error);
+        console.error("500 test", error);
         self.auth.logOut();
       }
     },
@@ -49,14 +50,14 @@ export const Calendar = types
     },
     startCreating() {
       self.creating = true;
-    }
+    },
   }));
 
 const Root = types.model({
   auth: types.optional(Auth, {}),
-  calendar: types.optional(Calendar, {})
+  calendar: types.optional(Calendar, {}),
 });
 
 export const store = Root.create({
-  auth: { loggedIn: false }
+  auth: { loggedIn: false },
 });
