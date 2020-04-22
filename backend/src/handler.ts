cac5300a400492bbe0b0dfd1a180a286eaa48943
jchung201 <jchung201@gmail.com>
2020-04-21 21:49:36 -0400

    (Serverless) Variable setup.

---
+++ backend/src/handler.ts
@@ -0,0 +1,17 @@
+// Import Express Instance
+import app from './lib/app';
+
+import router from './routes/router';
+app.use('/rest', router);
+
+// Catch all 404 if not found
+import httpErrors from 'http-errors';
+app.use((_req, _res, next) => {
+  next(httpErrors(404, '404 Not Found!'));
+});
+
+import errorHandler from './lib/errorHandler';
+app.use(errorHandler);
+
+import serverlessHttp from 'serverless-http';
+module.exports.handler = serverlessHttp(app);
