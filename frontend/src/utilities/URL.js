347fccd5a29e5317d79751b72ec41d242e6383c6
jchung201 <jchung201@gmail.com>
2020-04-21 21:32:50 -0400

    (Serverless) API usage.{'

--- frontend/src/utilities/URL.js
+++ frontend/src/utilities/URL.js
@@ -4,7 +4,8 @@ export let WEB_URL = "";
 switch (process.env.NODE_ENV) {
   case "production":
     WEB_URL = "https://www.birthday.watch";
-    API_URL = "https://birthday-watch.herokuapp.com";
+    API_URL = "https://z911u4qxd5.execute-api.us-east-1.amazonaws.com/dev";
+    // API_URL = "https://birthday-watch.herokuapp.com";
     break;
   default:
     WEB_URL = "http://localhost:8080";
